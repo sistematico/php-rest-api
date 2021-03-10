5f9c02a684cbd1dd124f4edc3bc2e16ec5034d14
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-10 14:34:14 -0400

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ list }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data() {
+    return {
+      list: 'Lista',
+    };
+  },
+  created: function () {
+    this.fetchUsers()
+  },
+  methods: {
+    fetchUsers: function () {
+      fetch("https://api.lucasbrum.net").then((response) => {
+        return response.json().then((json) => {
+          console.log("JSON", json);
+          this.list = json
+        })
+      })
+    }
   }
 }
 </script>
