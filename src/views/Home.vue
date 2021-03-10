664282890a1fd8960c3cfb9c11124e4d1273e69a
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-10 16:38:22 -0400

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,12 +30,12 @@ export default {
   },
   methods: {
     fetchUsers: function () {
-      // let headers = new Headers()
-      // headers.append('Access-Control-Allow-Origin', 'https://rest.lucasbrum.net');
-      // headers.append('Access-Control-Allow-Credentials', 'true');
-      // let options = { method: 'GET', headers: headers, mode: 'cors'}
+      let headers = new Headers()
+      headers.append('Access-Control-Allow-Origin', 'https://rest.lucasbrum.net');
+      headers.append('Access-Control-Allow-Credentials', 'true');
+      let options = { method: 'GET', headers: headers, mode: 'cors'}
       
-      fetch("https://api.lucasbrum.net/user/list")
+      fetch("https://api.lucasbrum.net/user/list", options)
         .then(json => {
           console.log(json)
           this.lista = JSON.stringify(json.data)
