2b2faeb342b26c0a43b2f35f774450de5ebfb3ef
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-11 12:55:14 -0400

    Update

--- public/index.php
+++ public/index.php
@@ -16,13 +16,14 @@
 
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
     header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     header("HTTP/1.1 200 OK");
     die();
 }
 
 // required headers
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://rest.lucasbrum.net');
+//header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
