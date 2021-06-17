ea25dc0d50ab571ea5d87c916426cdb18ea85f07
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-06-17 15:46:02 -0400

    Autocommit por GitHub: alterações: add 'src/router/index.js' add 'src/components/Pagination.vue' add 'src/views/Adicionar.vue' add 'src/views/Inicio.vue'

---
+++ src/views/Adicionar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="starter-template text-center py-5 px-3 home">
+    <h1>Adicionar Usuário</h1>
+
+    <div :class="alertClass" role="alert" v-if="mensagem">
+      {{ mensagem }}
+    </div>
+
+    <form class="row g-3 justify-content-center" @submit.prevent="addUser()" v-if="btnText !== 'Adicionado'">
+      <div class="col-auto">
+        <label for="nome" class="visually-hidden">Nome</label>
+        <input v-model="user.nome" type="text" class="form-control" id="nome" placeholder="Fulano de Tal">
+      </div>
+      <div class="col-auto">
+        <label for="usuario" class="visually-hidden">Usuário</label>
+        <input v-model="user.usuario" type="text" class="form-control" id="usuario" placeholder="usuario">
+      </div>
+      <div class="col-auto">
+        <label for="email" class="visually-hidden">E-mail</label>
+        <input v-model="user.email" type="text" class="form-control" id="email" placeholder="usuario@gmail.com">
+      </div>
+      <div class="col-auto">
+        <label for="senha" class="visually-hidden">Email</label>
+        <input v-model="user.senha" type="password" class="form-control" id="senha" placeholder="Senha" autocomplete="current-password">
+      </div>
+      <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-3" :disabled="btnText != 'Adicionar'">
+          <span class="spinner-border spinner-border-sm" role="status" aria-hidden="false" v-if="btnText === 'Processando...'"></span>
+          {{ btnText }}
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+import { ApiHandler } from "@/js/api";
+
+export default {
+  name: "Adicionar",
+  data() {
+    return {
+      loading: false,
+      btnText: 'Adicionar',
+      user: { nome: '', usuario: '', email: '', senha: '' },
+      mensagem: '',
+      alertClass: 'alert alert-primary'
+    }
+  },
+  methods: {
+    addUser() {
+      ApiHandler.add(this, this.user.nome, this.user.usuario, this.user.email, this.user.senha)
+      //ApiHandler.fetch(this)
+    }
+  }
+}
+</script>
---
+++ src/views/Inicio.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="starter-template text-center py-5 px-3 home">
+    <h1>Usuários</h1>
+
+    <div :class="alertClass" role="alert" v-if="mensagem">
+      {{ mensagem }}
+    </div>
+
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Usuário</th>
+        <th scope="col">E-mail</th>
+        <th scope="col">Apagar</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in registrosPaginados" :key="item">
+        <td scope="row">{{ item.id }}</td>
+        <td>{{ item.fullname }}</td>
+        <td>{{ item.username }}</td>
+        <td>{{ item.email }}</td>
+        <td><a href="#" @click.prevent="delUser(item.id)">del</a></td>
+      </tr>
+      </tbody>
+    </table>
+    <Pagination @to-parent="fromChild" :registros="registros" ref="pagination" />
+  </div>
+</template>
+<script>
+import Pagination from '@/components/Pagination.vue'
+import { ApiHandler } from "@/js/api";
+
+export default {
+  name: "Home",
+  components: { Pagination },
+  data() {
+    return {
+      registros: [],
+      registrosPaginados: [],
+      user: { nome: '', usuario: '', email: '', senha: '' },
+      mensagem: '',
+      alertClass: 'alert alert-primary',
+      timestamp: 0
+    };
+  },
+  mounted() {
+    ApiHandler.fetch(this)
+  },
+  methods: {
+    fromChild(value) {
+      this.registrosPaginados = value
+    },
+    delUser(id) {
+      ApiHandler.del(this, id)
+    }
+  },
+  watch: {
+    // pagina: function () {
+    //   this.paginacao()
+    // },
+    registros: function() {
+      ApiHandler.fetch(this)
+    }
+  }
+}
+</script>
